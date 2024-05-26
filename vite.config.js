88ea50d6d9f54ca1994f66cbb3598802b7e0221a
chronosgit <warfacemeister@mail.ru>
2024-05-26 14:31:56 +0500

    feat: updated configs for path aliases resolving

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@common": path.resolve(__dirname, "./src/common"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+    },
+  },
 })
