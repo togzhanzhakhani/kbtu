b91f3240f4365bb51845b5e591ebffbf14bc57fc
chronosgit <warfacemeister@mail.ru>
2024-05-26 19:11:13 +0500

    feat: started working on HomePartners

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -2,6 +2,7 @@ import HomeAbout from "./components/about/HomeAbout";
 import HomeDirections from "./components/directions/HomeDirections";
 import HomeHero from "./components/hero/HomeHero";
 import HomeImageSection from "./components/image_section/HomeImageSection";
+import HomePartners from "./components/partners/HomePartners";
 import styles from "./home.module.css";
 
 const Home = () => {
@@ -15,6 +16,8 @@ const Home = () => {
 			<HomeDirections />
 
 			<HomeImageSection />
+
+			<HomePartners />
 		</div>
 	);
 };
---
+++ src/modules/home/components/partners/HomePartners.jsx
@@ -0,0 +1,17 @@
+import Container from "@common/components/container/Container";
+import styles from "./home_partners.module.css";
+
+const HomePartners = () => {
+
+	return (
+		<section className={styles.section}>
+			<Container maxWidth="80rem" isCentered>
+				<h3 className={styles.heading}>Партнеры</h3>
+
+				
+			</Container>
+		</section>
+	);
+};
+
+export default HomePartners;
