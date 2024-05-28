361ffcd52e9a32981f7082cfbd6f303562c75e51
chronosgit <warfacemeister@mail.ru>
2024-05-28 22:51:05 +0500

    feat: added partnersLogos.js data for partners section in home page

---
+++ src/modules/home/utils/partnersLogos.js
@@ -0,0 +1,45 @@
+
+const partnersLogos = [
+	{
+		id: 1,
+		name: 'Stryde',
+		imageUrl: '/src/assets/images/logo_stryde.png',
+	},
+	{
+		id: 2,
+		name: 'Samga',
+		imageUrl: '/src/assets/images/logo_samga.png',
+	},
+	{
+		id: 3,
+		name: 'Schneider',
+		imageUrl: '/src/assets/images/logo_schneider.png',
+	},
+	{
+		id: 3,
+		name: 'Schneider',
+		imageUrl: '/src/assets/images/logo_schneider.png',
+	},
+	{
+		id: 4,
+		name: 'Baker Hughes',
+		imageUrl: '/src/assets/images/logo_baker.png',
+	},
+	{
+		id: 5,
+		name: 'Axellect',
+		imageUrl: '/src/assets/images/logo_axellect.png',
+	},
+	{
+		id: 6,
+		name: 'Halyk',
+		imageUrl: '/src/assets/images/logo_halyk.png',
+	},
+	{
+		id: 7,
+		name: 'Honeywell',
+		imageUrl: '/src/assets/images/logo_honeywell.png',
+	},
+];
+
+export default partnersLogos;
