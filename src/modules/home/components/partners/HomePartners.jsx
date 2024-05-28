3b2c8b526452d2171d3395e4efee3b46920f97fa
chronosgit <warfacemeister@mail.ru>
2024-05-28 23:32:41 +0500

    feat: added partners section at home page

--- src/modules/home/components/partners/HomePartners.jsx
+++ src/modules/home/components/partners/HomePartners.jsx
@@ -1,5 +1,6 @@
 import Container from '@common/components/container/Container';
 import styles from './home_partners.module.css';
+import partnersLogos from '@modules/home/utils/partnersLogos';
 
 const HomePartners = () => {
 
@@ -12,7 +13,20 @@ const HomePartners = () => {
 		>
 			<h3 className={styles.heading}>Партнеры</h3>
 
-			
+			<div className={styles.logos}>
+				{
+					partnersLogos.map((p, i) => {
+						return (
+							<img 
+								key={p.id == null ? i : p.id}
+								src={p.imageUrl}
+								alt={p.alt || ''}
+								className={styles.logo}
+							/>
+						)
+					})
+				}
+			</div>
 		</Container>
 	);
 };
