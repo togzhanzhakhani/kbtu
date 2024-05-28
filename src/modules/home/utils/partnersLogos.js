f650752acd6898c48c54bed1897e22cbd21eef61
chronosgit <warfacemeister@mail.ru>
2024-05-28 22:53:49 +0500

    feat: updated partnersLogos.js data with personal alt tags

--- src/modules/home/utils/partnersLogos.js
+++ src/modules/home/utils/partnersLogos.js
@@ -3,41 +3,43 @@ const partnersLogos = [
 	{
 		id: 1,
 		name: 'Stryde',
+		alt: 'stryde logo',
 		imageUrl: '/src/assets/images/logo_stryde.png',
 	},
 	{
 		id: 2,
 		name: 'Samga',
+		alt: 'samga logo',
 		imageUrl: '/src/assets/images/logo_samga.png',
 	},
 	{
 		id: 3,
 		name: 'Schneider',
-		imageUrl: '/src/assets/images/logo_schneider.png',
-	},
-	{
-		id: 3,
-		name: 'Schneider',
+		alt: 'schneider logo',
 		imageUrl: '/src/assets/images/logo_schneider.png',
 	},
 	{
 		id: 4,
 		name: 'Baker Hughes',
+		alt: 'baker hughes logo',
 		imageUrl: '/src/assets/images/logo_baker.png',
 	},
 	{
 		id: 5,
 		name: 'Axellect',
+		alt: 'axellect logo',
 		imageUrl: '/src/assets/images/logo_axellect.png',
 	},
 	{
 		id: 6,
 		name: 'Halyk',
+		alt: 'halyk bank logo',
 		imageUrl: '/src/assets/images/logo_halyk.png',
 	},
 	{
 		id: 7,
 		name: 'Honeywell',
+		alt: 'honeywell logo',
 		imageUrl: '/src/assets/images/logo_honeywell.png',
 	},
 ];
