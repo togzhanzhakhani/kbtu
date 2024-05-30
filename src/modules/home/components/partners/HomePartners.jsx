0e2b933d32ba71ff32bf575cc1f13b9c6c9a15ae
chronosgit <warfacemeister@mail.ru>
2024-05-30 19:52:35 +0500

    feat: transformed partners section into an infinite slider

--- src/modules/home/components/partners/HomePartners.jsx
+++ src/modules/home/components/partners/HomePartners.jsx
@@ -1,6 +1,7 @@
 import Container from '@common/components/container/Container';
 import styles from './home_partners.module.css';
 import partnersLogos from '@modules/home/utils/partnersLogos';
+import mergeClassnames from '@common/utils/mergeClassnames';
 
 const HomePartners = () => {
 
@@ -14,18 +15,35 @@ const HomePartners = () => {
 			<h3 className={styles.heading}>Партнеры</h3>
 
 			<div className={styles.logos}>
-				{
-					partnersLogos.map((p, i) => {
-						return (
-							<img 
-								key={p.id == null ? i : p.id}
-								src={p.imageUrl}
-								alt={p.alt || ''}
-								className={styles.logo}
-							/>
-						)
-					})
-				}
+				<div className={styles.logos_slide}>
+					{
+						partnersLogos.map((p, i) => {
+							return (
+								<img 
+									key={p.id == null ? i : p.id}
+									src={p.imageUrl}
+									alt={p.alt || ''}
+									className={styles.logo}
+								/>
+							)
+						})
+					}
+				</div>
+
+				<div className={styles.logos_slide} aria-hidden >
+					{
+						partnersLogos.map((p, i) => {
+							return (
+								<img 
+									key={p.id == null ? i : p.id}
+									src={p.imageUrl}
+									alt={p.alt || ''}
+									className={styles.logo}
+								/>
+							)
+						})
+					}
+				</div>
 			</div>
 		</Container>
 	);
