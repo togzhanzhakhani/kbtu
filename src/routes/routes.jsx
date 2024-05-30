a1548bc7b6d2015bd704c5f1d32f43d776315fea
chronosgit <warfacemeister@mail.ru>
2024-05-30 19:57:49 +0500

    feat: resolved merge

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,3 +1,4 @@
+import PersonalCabinet from "@modules/PersonalCabinet/PersonalCabinet";
 import Login from "@modules/authorization/Login";
 import Registration from "@modules/authorization/Registration";
 import Home from "@modules/home/Home";
@@ -14,6 +15,10 @@ const routes = [
 	{
 		path: "register",
 		element: <Registration />
+	},
+	{
+		path: "/profile",
+		element: <PersonalCabinet />
 	}
 ];
 
