db9b728251a9329f50bfbec3663ecf8692e9c55a
chronosgit <warfacemeister@mail.ru>
2024-06-02 12:04:37 +0500

    feat: updated home directionsdData

--- src/modules/home/utils/directionsData.js
+++ src/modules/home/utils/directionsData.js
@@ -1,62 +1,62 @@
 const directionsData = [
 	{
 		id: 1,
-		imageUrl: "/src/assets/icons/home_directions_stonks.svg",
+		imageUrl: "/src/assets/icons/stonks.svg",
 		title: "Бизнес-обучение",
 	},
 	{
 		id: 2,
-		imageUrl: "/src/assets/icons/home_directions_tube.svg",
+		imageUrl: "/src/assets/icons/tube.svg",
 		title: "Химические разработки",
 	},
 	{
 		id: 3,
-		imageUrl: "/src/assets/icons/home_directions_energy_efficiency.svg",
+		imageUrl: "/src/assets/icons/energy_efficiency.svg",
 		title: "Оптимизация и энергоэффективность",
 	},
 	{
 		id: 4,
-		imageUrl: "/src/assets/icons/home_directions_ecology.svg",
+		imageUrl: "/src/assets/icons/ecology.svg",
 		title: "Экология и ESG",
 	},
 	{
 		id: 5,
-		imageUrl: "/src/assets/icons/home_directions_security.svg",
+		imageUrl: "/src/assets/icons/security.svg",
 		title: "Промышленная безопасность",
 	},
 	{
 		id: 6,
-		imageUrl: "/src/assets/icons/home_directions_oil_pump.svg",
+		imageUrl: "/src/assets/icons/oil_pump.svg",
 		title: "Повышение нефтеотдачи",
 	},
 	{
 		id: 7,
-		imageUrl: "/src/assets/icons/home_directions_digitalization.svg",
+		imageUrl: "/src/assets/icons/digitalization.svg",
 		title: "ИТ/Цифровизация",
 	},
 	{
 		id: 8,
-		imageUrl: "/src/assets/icons/home_directions_geology.svg",
+		imageUrl: "/src/assets/icons/geology.svg",
 		title: "Геология и сейсмология",
 	},
 	{
 		id: 9,
-		imageUrl: "/src/assets/icons/home_directions_hydro_energy.svg",
+		imageUrl: "/src/assets/icons/hydro_energy.svg",
 		title: "Водородная энергетика",
 	},
 	{
 		id: 10,
-		imageUrl: "/src/assets/icons/home_directions_corrosion.svg",
+		imageUrl: "/src/assets/icons/corrosion.svg",
 		title: "Проблемы коррозии",
 	},
 	{
 		id: 11,
-		imageUrl: "/src/assets/icons/home_directions_science.svg",
+		imageUrl: "/src/assets/icons/science.svg",
 		title: "Технологии и моделирование",
 	},
 	{
 		id: 12,
-		imageUrl: "/src/assets/icons/home_directions_material_science.svg",
+		imageUrl: "/src/assets/icons/material_science.svg",
 		title: "Материаловедение",
 	},
 ];	
