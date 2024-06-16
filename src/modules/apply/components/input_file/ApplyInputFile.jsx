4f3cb072c2d4bee927d56d63fc1c609452e11a3b
chronosgit <warfacemeister@mail.ru>
2024-06-16 14:47:48 +0500

    feat: updated apply form

--- src/modules/apply/components/input_file/ApplyInputFile.jsx
+++ src/modules/apply/components/input_file/ApplyInputFile.jsx
@@ -2,10 +2,16 @@ import RequirementStar from '@common/components/requirement_star/RequirementStar
 import doExist from '@common/utils/doExist';
 import clipIcon from '@assets/icons/clip.svg';
 import styles from './apply_input_file.module.css';
+import { useState } from 'react';
+import { FaFilePdf } from 'react-icons/fa';
+import removeLastChars from '@common/utils/removeLastChars';
 
 const ApplyInputFile = ({
 	label, isRequired = false, onChange
 }) => {
+
+	const [fileName, setFileName] = useState('');
+	const [error, setError] = useState('');
 
 	if(!doExist(label, onChange)) {
 		return;
@@ -15,9 +21,27 @@ const ApplyInputFile = ({
 
 	// TODO: finish
 	const loadFile = (e) => {
+		setError('');
+
 		const file = e.target.files[0];
 
     if(file) {
+			const fileType = file.type;
+			const fileSize = file.size;
+			console.log(fileSize / 1e6);
+
+			if(fileType !== 'application/pdf') {
+				setError('Файл должен быть PDF');
+				return;
+			}
+
+			if(fileSize > 5) {
+				setError('Размер файла не должен превышать 5МБ');
+				return;
+			}
+
+			setFileName(removeLastChars(file.name, 4));
+
 			const reader = new FileReader();
 
 			reader.onload = (e) => {
@@ -30,7 +54,9 @@ const ApplyInputFile = ({
 			};
 
 			reader.readAsDataURL(file);
-    }
+    } else {
+			setError('Файл должен быть загружен');
+		}
 	};
 
 	return (
@@ -46,23 +72,46 @@ const ApplyInputFile = ({
 				{isRequired && <RequirementStar />}
 			</div>
 
-			<div className={styles.file_box}>
-				<img 
-					src={clipIcon} 
-					alt="clip icon" 
-					className={styles.clip_icon}
-				/>
+			<div className={styles.wrapper}>
+				<div className={styles.file_box}>
+					<img 
+						src={clipIcon} 
+						alt="clip icon" 
+						className={styles.clip_icon}
+					/>
 
-				<p className={styles.name}>
-					{label}
-				</p>
+					<p className={styles.name}>
+						{label}
+					</p>
 
-				<input
-					id={inputId}
-					type='file'
-					className={styles.file_input}
-					onChange={loadFile}
-				/>
+					<input
+						id={inputId}
+						type='file'
+						className={styles.file_input}
+						onChange={loadFile}
+					/>
+				</div>
+
+				{
+					error.length > 0 &&
+					<p className={styles.error}>
+						{error}
+					</p>
+				}
+
+				{
+					fileName.length > 0 &&
+					<div className={styles.file_preview}>
+						<FaFilePdf 
+							fontSize='1.5rem'
+							color='#e95340'
+						/>
+
+						<p className={styles.file_name}>
+							{fileName}
+						</p>
+					</div>
+				}
 			</div>
 		</div>
 	);
